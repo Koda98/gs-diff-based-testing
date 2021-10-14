586e97b3a594e482f9a6081d57f031882a59f5e3
Kyle Oda <1624udon@gmail.com>
2021-10-14 03:31:30 -0700

    uses python3.9 command

--- reference/diffs.sh
+++ reference/diffs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # @test{"stdout":20,"name":"You should echo foo on stdout"}
-python3 hello.py 
+python3.9 hello.py
 
 # @test{"stderr":10, "name":"check stderr"}
-python3 hello.py
+python3.9 hello.py
 
 
 
